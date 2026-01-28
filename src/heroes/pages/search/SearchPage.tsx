88848d1f0701a378cb690587520237902d8f749b
vCentDev <vicentDev@yahoo.com>
2026-01-28 20:08:37 +0100

    feat(search): enhance SearchPage with HeroStats and SearchControls components for improved hero search functionality

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,10 +1,19 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
+import { HeroStats } from "@/heroes/components/HeroStats"
+import { SearchControls } from "./ui/SearchControls"
 
 export const SearchPage = () => {
     return (
-        <CustomJumbotron
-            title="Búsqueda de SuperHéroes"
-            subtitle="Descubre, explora y administra super héroes y villanos"
-        />
+        <>
+            <CustomJumbotron
+                title="Búsqueda de SuperHéroes"
+                subtitle="Descubre, explora y administra super héroes y villanos"
+            />
+            {/* Stats dashboards */}
+            <HeroStats />
+
+            {/* Filter and search */}
+            <SearchControls />
+        </>
     )
 }
