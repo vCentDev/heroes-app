755ec6fbec46f39bd6e0e37344aa5b740d100937
vCentDev <vicentDev@yahoo.com>
2026-01-28 20:08:27 +0100

    feat(heroes): introduce HeroGrid and HeroStatCard components; refactor HomePage to utilize new components and enhance layout

---
+++ src/heroes/components/HeroStatCard.tsx
@@ -0,0 +1,21 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import type { JSX, PropsWithChildren } from "react";
+
+interface Props extends PropsWithChildren {
+    title: string;
+    icon: JSX.Element;
+}
+
+export const HeroStatCard = ({ title, icon, children }: Props) => {
+    return (
+        <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">{title}</CardTitle>
+                {icon}
+            </CardHeader>
+            <CardContent>
+                {children}
+            </CardContent>
+        </Card>
+    )
+}
