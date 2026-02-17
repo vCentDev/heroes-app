3e7203f62913afa4c122de4fe5746ab1ddbd52ef
vCentDev <vicentDev@yahoo.com>
2026-02-17 18:18:25 +0100

    feat(tests): add Vitest configuration and initial tests for hero API and actions, include .env.test for API URL configuration

---
+++ src/heroes/actions/get-hero.action.test.ts
@@ -0,0 +1,5 @@
+import { describe, test } from 'vitest';
+
+describe('getHeroAction', () => {
+  test('should fetch hero data and return with complete imgae ulr', () => {});
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
