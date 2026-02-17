6fee24426bdf9c33965937592f18bea36f60fe00
vCentDev <vicentDev@yahoo.com>
2026-02-17 21:25:56 +0100

    test(getHeroAction): add comprehensive tests for fetching hero data and handling errors

--- src/heroes/actions/get-hero.action.test.ts
+++ src/heroes/actions/get-hero.action.test.ts
@@ -1,5 +1,48 @@
-import { describe, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
+import { getHeroAction } from './get-hero.action';
 
 describe('getHeroAction', () => {
-  test('should fetch hero data and return with complete imgae ulr', () => {});
+  test('should fetch hero data and return with complete image url', async () => {
+    const result = await getHeroAction('clark-kent');
+
+    expect(result.image).toContain('http');
+
+    expect(result).toStrictEqual({
+      id: '1',
+      name: 'Clark Kent',
+      slug: 'clark-kent',
+      alias: 'Superman',
+      powers: [
+        'Súper fuerza',
+        'Vuelo',
+        'Visión de calor',
+        'Visión de rayos X',
+        'Invulnerabilidad',
+        'Súper velocidad',
+      ],
+      description:
+        'El Último Hijo de Krypton, protector de la Tierra y símbolo de esperanza para toda la humanidad.',
+      strength: 10,
+      intelligence: 8,
+      speed: 9,
+      durability: 10,
+      team: 'Liga de la Justicia',
+      image: 'http://localhost:3001/images/1.jpeg',
+      firstAppearance: '1938',
+      status: 'Active',
+      category: 'Hero',
+      universe: 'DC',
+    });
+  });
+
+  test('should throw an error if hero is not found', async () => {
+    const idSlug = 'batman-2';
+
+    const result = await getHeroAction(idSlug).catch((error) => {
+      expect(error).toBeDefined();
+      expect(error.message).toBe('Request failed with status code 404');
+    });
+
+    expect(result).toBeUndefined();
+  });
 });
